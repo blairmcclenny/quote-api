e13e2eaf7c328a1b2f32239d6fd889930c1ef2fe
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-07-22 16:10:47 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+
+const { quotes } = require("./data")
+const { getRandomElement } = require("./utils")
+
+const PORT = process.env.PORT || 4001
+
+app.use(express.static("public"))
+
+app.get("/api/quotes/random", (req, res) => {
+  res.send({ quote: getRandomElement(quotes) })
+})
+
+app.get("/api/quotes", (req, res) => {
+  const person = req.query.person
+
+  if (person !== undefined) {
+    res.send({ quotes: quotes.filter((q) => person === q.person) })
+  } else {
+    res.send({ quotes })
+  }
+})
+
+app.post("/api/quotes", (req, res) => {
+  const quote = {
+    quote: req.query.quote,
+    person: req.query.person,
+  }
+
+  if (quote.quote && quote.person) {
+    quotes.push(quote)
+    res.send({ quote })
+  } else {
+    res.status(400).send()
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
